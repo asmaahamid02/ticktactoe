1f1803585831ae80d7abff30939367accc997131
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-16 22:53:40 +0300

    refactore js code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -25,52 +25,75 @@ document.addEventListener('DOMContentLoaded', () => {
     [2, 4, 6],
   ]
 
-  let won = false
+  const isClickValid = (card) => {
+    const span_hidden_text = card.childNodes[0]
+    if (
+      span_hidden_text === 'red' ||
+      span_hidden_text === 'Yellow' ||
+      !isActive
+    ) {
+      return false
+    }
+    return true
+  }
+
+  const checkWinner = () => {
+    let won = false
+    for (let i = 0; i < indices_winning_conditions.length; i++) {
+      //get the winning case based on index
+      const winning_case = indices_winning_conditions[i]
+      const first = boxes[winning_case[0]]
+      const second = boxes[winning_case[1]]
+      const third = boxes[winning_case[2]]
+
+      //check if the boxes are not clicked
+      if (first === '' || second === '' || third === '') {
+        continue
+      }
+
+      //check if the same user clicked the cards
+      if (first === second && second === third) {
+        won = true
+        break
+      }
+    }
+
+    //display text for winner
+    if (won) {
+      controls.innerHTML =
+        current_player === 'red'
+          ? '<span class="red-player">Red</span> Player Won'
+          : '<span class="yellow-player">Yellow</span> Player Won'
+
+      isActive = false
+
+      controls.classList.remove('hide')
+    }
+
+    if (!boxes.includes('')) {
+      controls.innerHTML = 'Tie'
+    }
+  }
+
+  //change player controls
+  const changePlayer = () => {
+    display_player.classList.remove(`${current_player}-player`)
+    current_player = current_player === 'red' ? 'yellow' : 'red'
+    display_player.textContent = current_player
+    display_player.classList.add(`${current_player}-player`)
+  }
+
   const play = (card, index) => {
     const span_hidden_text = card.childNodes[0]
-    if (span_hidden_text === null || !isActive) {
-      return
-    } else {
+    if (isClickValid(card)) {
       card.classList.add(current_player)
       span_hidden_text.textContent = current_player
       boxes[index] = current_player
 
-      for (let i = 0; i < indices_winning_conditions.length; i++) {
-        const winning_case = indices_winning_conditions[i]
-        const first = boxes[winning_case[0]]
-        const second = boxes[winning_case[1]]
-        const third = boxes[winning_case[2]]
-        console.log(first, second, third)
-        if (first === '' || second === '' || third === '') {
-          continue
-        }
-
-        if (first === second && second === third) {
-          won = true
-          break
-        }
-      }
-
-      if (won) {
-        controls.innerHTML =
-          current_player === 'red'
-            ? '<span class="red-player">Red</span> Player Won'
-            : '<span class="yellow-player">Yellow</span> Player Won'
-
-        isActive = false
-
-        controls.classList.remove('hide')
-      }
-
-      if (!boxes.includes('')) {
-        controls.innerHTML = 'Tie'
-      }
-
-      display_player.classList.remove(`${current_player}-player`)
-      current_player = current_player === 'red' ? 'yellow' : 'red'
-      display_player.textContent = current_player
-      display_player.classList.add(`${current_player}-player`)
-      console.log(won, current_player, controls.textContent)
+      checkWinner()
+      changePlayer()
+    } else {
+      return
     }
   }
 
